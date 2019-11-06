153304ee96a1d4abc999f8550e404b0e8788ac39
Daniel Cataño <dcatano@cidenet.com.co>
2019-11-06 10:18:33 -0500

    Tipos de datos

---
+++ types.dart
@@ -0,0 +1,12 @@
+main() {
+    //int, double, String, bool, dynamic
+    int a = 1;
+    double b = 2.5;
+    String c = 'c';
+    bool d = true;
+    dynamic e = 1;
+    print('$a, $b, $c, $d, $e');
+    d = false;
+    e = 'e';
+    print('$a, $b, $c, $d, $e');
+}
